112cd2fccc2824f222fb48368b04c59056798a2d
Lelik Jan <21alex7777@gmail.com>
2023-06-30 18:30:24 +0300

    feat: add styles for pagination

--- src/js/pagination.js
+++
@@ -1,8 +0,0 @@
-console.log("hello from pagination");
-
-const sectionPagination = document.querySelector(".section__pagination");
-const div = document.createElement("div");
-const img=document.createElement('img');
-img.src='../assets/pets-charly.webp';
-sectionPagination && sectionPagination.append(div);
-img&&div.append(img);
