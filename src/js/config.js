7bbee1ffc3726f9bb7b354ac79c6c4a47d636c08
Lelik Jan <21alex7777@gmail.com>
2023-06-30 19:26:36 +0300

    feat:add config.js and utils.js

---
+++ src/js/config.js
@@ -0,0 +1,17 @@
+const CLASSES = {
+  SECTION_SLIDER: ".section__slider",
+  SECTION_PAGINATION: ".section__pagination",
+  BUTTON_LEFT: ".button_left",
+  BUTTON_RIGHT: ".button_right",
+  PAGINATION_CARDS:'.pagination__cards',
+  CARD_IMG:'.card__img',
+  CARD__BUTTON:'.card__button',
+  CARD:'.card',
+  CARDS:'.cards',
+  PAGINATION_BUTTONS:'.pagination__buttons',
+  BUTTON:'.button',
+  SLIDER:'.slider',
+  CARDS_CONTAINER:'.cards__container',
+};
+const CONSTANTS = {};
+export { CLASSES, CONSTANTS };
